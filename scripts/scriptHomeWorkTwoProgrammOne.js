f218bbc55bb8eb95418053ad5d575f3736b0e113
Dunkan <deeeeeeeenchik@yandex.ru>
2019-08-21 09:12:20 +0300

    correction of the first program of the second module

--- scripts/scriptHomeWorkTwoProgrammOne.js
+++ scripts/scriptHomeWorkTwoProgrammOne.js
@@ -1,15 +1,17 @@
 alert('Какое число больше?');
 var numberFirst = prompt('Введите первое число');
 var numberSecond = prompt('Введите второе число');
+var numFirst = Number(numberFirst);
+var numSecond = Number(numberSecond);
 if(numberFirst == '' || numberSecond == '') {
   alert('Пустая строка!');
 } else if(numberFirst == null || numberSecond == null) {
   alert('Вы нажали отмена!');
 } else if(!numberFirst.replace(/[^-0-9]/gim,'') || !numberSecond.replace(/[^-0-9]/gim,'')) {
   alert('Это не число!');
-} else if(numberFirst > numberSecond) {
+} else if(numFirst > numSecond) {
   alert('Первое число больше второго');
-} else if(numberFirst < numberSecond) {
+} else if(numFirst < numSecond) {
   alert('Второе число больше первого');
 } else {
   alert('Числа равны!');
