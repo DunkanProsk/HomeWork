55795f16fc91519dd312f5184bf1098cc9307065
DunkanProsk <deeeeeeeenchik@yandex.ru>
2019-08-24 17:18:57 +0300

    adjustment of the first program of the third module

--- scripts/scriptHomeWorkThreeProgrammOne.js
+++ scripts/scriptHomeWorkThreeProgrammOne.js
@@ -1,7 +1,25 @@
-alert('Угадывание числа, методом - больше, меньше');
-
-var i = prompt('Угадай число, которое я загадал');
+alert('Угадайте число от 0 до 1000, методом - больше, меньше');
 var j = getRandomInt(0, 1000);
+var x = 0;
+
+for(var n = 0; n < 5; n++) {
+	if(n <= 3) {
+		if(x !== 1) {
+			i = prompt('Число');
+			num(i, j);
+		} else {
+			break;
+		}
+	} else {
+		if(x !== 1) {
+			alert('Последняя попытка');
+			i = prompt('Число');
+			numEnd(i, j);
+		} else {
+			break;
+		}
+	}
+}	
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -10,21 +28,43 @@ function getRandomInt(min, max) {
 function num(i, j) {
 	if(i === null) {
 		alert('Отмена!');
+		return x = 1;
 	} else if(i == '') {
 		alert('Пустая строка!');
-	} else if(i === undefined){
-		alert('Некорректные данные!');
-	}else if(i > j) {
+		return x = 1;
+	} else if(i > j) {
 		alert('Меньше!');
-		num(i = prompt('Еще попытка!'), j);
+		return x = 2;
 	} else if(i < j) {
 		alert('Больше!');
-		num(i = prompt('Еще попытка!'), j);
+		return x = 2;
 	} else if(i == j) {
 		alert('Молодец!');
+		return x = 1;
 	} else {
 		alert('Некорректные данные!');
+		return x = 1;
 	}
 }
 
-num(i, j);
+function numEnd(i, j) {
+	if(i === null) {
+		alert('Отмена!');
+		return x = 1;
+	} else if(i == '') {
+		alert('Пустая строка!');
+		return x = 1;
+	} else if(i > j) {
+		alert('Неверно!');
+		return x = 2;
+	} else if(i < j) {
+		alert('Неверно!');
+		return x = 2;
+	} else if(i == j) {
+		alert('Молодец!');
+		return x = 1;
+	} else {
+		alert('Некорректные данные!');
+		return x = 1;
+	}
+}
