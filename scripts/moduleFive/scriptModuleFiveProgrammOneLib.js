265161082197034e60ef45fab8c43808bcd15739
DunkanProsk <deeeeeeeenchik@yandex.ru>
2019-09-04 01:32:50 +0300

    added fifth module first programm

---
+++ scripts/moduleFive/scriptModuleFiveProgrammOneLib.js
@@ -0,0 +1,86 @@
+
+function start() {
+	var x = 0;
+	var randomazer = genRandomNumber();
+
+	for(var n = 0; n < 10; n++) {
+	if(n <= 8) {
+		if(x !== 1) {
+			i = prompt('Число');
+			num(i, randomazer);
+		} else {
+			break;
+		}
+	} else {
+		if(x !== 1) {
+			alert('Последняя попытка');
+			i = prompt('Число');
+			numEnd(i, randomazer);
+		} else {
+			break;
+		}
+	}
+	}
+
+	questEnd();
+
+	function genRandomNumber() {
+		let random = 0;
+		return function() {
+			return random = Math.floor(Math.random() * (1000 - 0)) + 0;
+		}
+	}
+
+	function num(i, j) {
+		if(i === null) {
+			alert('Отмена!');
+			return x = 1;
+		} else if(i == '') {
+			alert('Пустая строка!');
+			return x = 1;
+		} else if(i > j) {
+			alert('Меньше!');
+			return x = 2;
+		} else if(i < j) {
+			alert('Больше!');
+			return x = 2;
+		} else if(i == j) {
+			alert('Молодец!');
+			return x = 1;
+		} else {
+			alert('Некорректные данные!');
+			return x = 1;
+		}
+	}
+
+	function numEnd(i, j) {
+		if(i === null) {
+			alert('Отмена!');
+			return x = 1;
+		} else if(i == '') {
+			alert('Пустая строка!');
+			return x = 1;
+		} else if(i > j) {
+			alert('Неверно!');
+			return x = 2;
+		} else if(i < j) {
+			alert('Неверно!');
+			return x = 2;
+		} else if(i == j) {
+			alert('Молодец!');
+			return x = 1;
+		} else {
+			alert('Некорректные данные!');
+			return x = 1;
+		}
+	}
+
+	function questEnd() {
+		let quest = confirm('Хотите начать заново?');
+		if(quest === true) {
+			getNum();
+		} else {
+			alert('Хорошего дня!');
+		}
+	}
+}
