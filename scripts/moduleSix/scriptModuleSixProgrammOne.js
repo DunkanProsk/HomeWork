723521a07f344e5e6e8cf5e73ef9d23e877006a1
Dunkan <deeeeeeeenchik@yandex.ru>
2019-10-20 23:23:52 +0300

    cor module six programm one

--- scripts/moduleSix/scriptModuleSixProgrammOne.js
+++ scripts/moduleSix/scriptModuleSixProgrammOne.js
@@ -1,10 +1,13 @@
-function User(firstName, lastName){
-  this.firstName = firstName,
-  this.lastName = lastName,
-  this.regDate = getDate()
+function User(firstName, lastName) {
+    this.firstName = firstName,
+    this.lastName = lastName,
+    this.regDate = this.getDate(),
+    this.fullInfo = function() {
+  	   return this.firstName + ' ' + this.lastName + ' ' + this.regDate;
+    }
 }
 
-function getDate(){
+User.prototype.getDate = function() {
 	let globalDate = new Date(); 
 	let arrDay =  [ globalDate.getDate(),
 					globalDate.getMonth(),
@@ -13,11 +16,33 @@ function getDate(){
                     globalDate.getMinutes(),
                     globalDate.getSeconds() ];
     return String(arrDay[0]) + '.' +
-    	   String(arrDay[1]) + '.' + 
+    	   String(arrDay[1] + 1) + '.' + 
     	   String(arrDay[2]) + ' ' + 
     	   String(arrDay[3]) + ':' +
     	   String(arrDay[4]) + ':' +
     	   String(arrDay[5]);            
 }
 
-console.log(getDate());
+function UserList() {
+    this.ArrayUserList = [];
+
+    do {    
+        var fullName = prompt('Введите имя и фамилию пользователя');
+        if (fullName !== null) {
+            var arrFullName = fullName.split(' ');
+            var firstName = arrFullName[0];
+            var lastName = arrFullName[1];
+            this.ArrayUserList.push(new User(firstName, lastName));
+        }
+    } while (fullName !== null);
+
+    for(var obj of this.ArrayUserList) {
+        console.log(obj.fullInfo());
+    }
+}
+
+UserList.prototype.getAllUsers = function() {
+    console.log('asd');
+    for(var obj of this.ArrayUserList) {
+        console.log(obj.fullInfo());
+    }
