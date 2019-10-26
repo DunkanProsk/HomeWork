260a79cdd3c96596cd8c3e6baf5c7e069f82ea59
DunkanProsk <deeeeeeeenchik@yandex.ru>
2019-10-26 14:41:10 +0300

    module six prog one

--- scripts/moduleSix/scriptModuleSixProgrammOne.js
+++ scripts/moduleSix/scriptModuleSixProgrammOne.js
@@ -1,51 +1,54 @@
 function User(firstName, lastName) {
     this.firstName = firstName,
     this.lastName = lastName,
-    this.regDate = this.getDate(),
+    this.regDate = this.regDate(),
     this.fullInfo = function() {
-  	   return this.firstName + ' ' + this.lastName + ' ' + this.regDate;
+        return this.firstName + ' ' + this.lastName + ' ' + this.regDate;
     }
 }
 
-User.prototype.getDate = function() {
-	let globalDate = new Date(); 
-	let arrDay =  [ globalDate.getDate(),
-					globalDate.getMonth(),
+User.prototype.regDate = function() {
+    let globalDate = new Date(); 
+    let arrDay =  [ globalDate.getDate(),
+                    globalDate.getMonth(),
                     globalDate.getFullYear(),
                     globalDate.getHours(),
                     globalDate.getMinutes(),
                     globalDate.getSeconds() ];
     return String(arrDay[0]) + '.' +
-    	   String(arrDay[1] + 1) + '.' + 
-    	   String(arrDay[2]) + ' ' + 
-    	   String(arrDay[3]) + ':' +
-    	   String(arrDay[4]) + ':' +
-    	   String(arrDay[5]);            
+           String(arrDay[1] + 1) + '.' + 
+           String(arrDay[2]) + ' ' + 
+           String(arrDay[3]) + ':' +
+           String(arrDay[4]) + ':' +
+           String(arrDay[5]);            
 }
 
 function UserList() {
-    this.ArrayUserList = [];
+    this.ArrUserList = [];
 }
 
 UserList.prototype.getAllUsers = function() {
-    for(var obj of this.ArrayUserList) {
+    for(var obj of this.ArrUserList) {
         console.log(obj.fullInfo());
     }
 }
 
-UserList.prototype.add = function(firstName, lastName) {
-    this.ArrayUserList.push(new User(firstName, lastName));   
+UserList.prototype.add = function (firstName, lastName) {
+    this.ArrUserList.push(new User(firstName, lastName))
 }
 
-
-var userList = new UserList();
+var userList = new UserList(),
+    arrFullName,
+    fullName,
+    firstName,
+    lastName;
 
 do {    
-    var fullName = prompt('Введите имя и фамилию пользователя');
+    fullName = prompt('Введите имя и фамилию пользователя');
     if (fullName !== null) {
-        var arrFullName = fullName.split(' ');
-        var firstName = arrFullName[0];
-        var lastName = arrFullName[1];
+        arrFullName = fullName.split(' ');
+        firstName = arrFullName[0];
+        lastName = arrFullName[1];
         userList.add(firstName, lastName);
     }
 } while (fullName !== null);
